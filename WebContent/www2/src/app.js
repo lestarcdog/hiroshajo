d37ddba534844643599e0abc5a6406f5348cfb40
cdog <cdog@cdogmainframe>
2015-10-25 10:25:56 +0100

    header

--- WebContent/www2/src/app.js
+++ WebContent/www2/src/app.js
@@ -5,11 +5,15 @@ app.config(function($routeProvider, $locationProvider) {
 
 app
 		.run(function($rootScope) {
-			var page = {
+			var meta = {
 				title : "Üvegszálas műanyag hajó, csónak, horgászcsónak gyártás. " + (new Date).getFullYear(),
 				description : "A Híröshajó Kft. katamarán aljú, üvegszálas műanyag hajó, csónak, horgászcsónak tervezésével és gyártásával foglalkozik.",
 				keywords : "hajó, csónak, horgászcsónak, üvegszálas műanyag"
 
-			}
+			};
+			var page = {
+				title : "hajó, csónak, horgászcsónak"
+			};
+			$rootScope.meta = meta;
 			$rootScope.page = page;
 		})
---
+++ WebContent/www2/src/custom/custom.js
@@ -0,0 +1,1 @@
+$("#header-evszam").attr("src", "images/header/evszam" + (new Date).getFullYear() + ".png")
