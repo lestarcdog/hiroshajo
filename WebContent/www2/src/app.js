abce907b6da4e4fead89502e8b021eb1dda0e6ac
cdog <cdog@cdogmainframe>
2015-10-25 09:10:02 +0100

    angular project added

---
+++ WebContent/www2/src/app.js
@@ -0,0 +1,15 @@
+var app = angular.module("HiroshajoApp", [ "ngRoute" ]);
+app.config(function($routeProvider, $locationProvider) {
+	$locationProvider.html5Mode(true);
+});
+
+app
+		.run(function($rootScope) {
+			var page = {
+				title : "Üvegszálas műanyag hajó, csónak, horgászcsónak gyártás. " + (new Date).getFullYear(),
+				description : "A Híröshajó Kft. katamarán aljú, üvegszálas műanyag hajó, csónak, horgászcsónak tervezésével és gyártásával foglalkozik.",
+				keywords : "hajó, csónak, horgászcsónak, üvegszálas műanyag"
+
+			}
+			$rootScope.page = page;
+		})
