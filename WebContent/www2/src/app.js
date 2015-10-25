858a0fb9e7ced0cd1c9e020512d04d0596c91ef9
Csaba Lestar <csaba.lestar@gmail.com>
2015-10-25 22:56:39 +0100

    couple of pages and active path

--- WebContent/www2/src/app.js
+++ WebContent/www2/src/app.js
@@ -2,6 +2,12 @@ var app = angular.module("HiroshajoApp", [ "ngRoute" ]);
 app.config(function($routeProvider) {
 	$routeProvider.when("/", {
 		templateUrl : "views/main.html"
+	}).when("/used", {
+		templateUrl : "views/used.html"
+	}).when("/why_catamaran", {
+		templateUrl : "views/why_catamaran.html"
+	}).when("/contact", {
+		templateUrl : "views/contact.html"
 	}).otherwise({
 		redirectTo : '/'
 	});
@@ -10,7 +16,8 @@ app.config(function($routeProvider) {
 app
 		.run(function($rootScope) {
 			var meta = {
-				title : "Üvegszálas műanyag hajó, csónak, horgászcsónak gyártás. " + (new Date).getFullYear(),
+				title : "Üvegszálas műanyag hajó, csónak, horgászcsónak gyártás. "
+						+ (new Date).getFullYear(),
 				description : "A Híröshajó Kft. katamarán aljú, üvegszálas műanyag hajó, csónak, horgászcsónak tervezésével és gyártásával foglalkozik.",
 				keywords : "hajó, csónak, horgászcsónak, üvegszálas műanyag"
 
@@ -20,4 +27,10 @@ app
 			};
 			$rootScope.meta = meta;
 			$rootScope.page = page;
-		})
+		});
+
+app.controller("LocationCtrl",function($scope,$location) {
+	$scope.isActive = function(route) {
+		return $location.path() == route;
+	}
+});
