19db9213483abde9606d7612a072fdc7d0d20040
cdog <cdog@cdog-laptop>
2015-11-02 00:12:52 +0100

    collapse button start

--- WebContent/www2/src/controllers/boatController.js
+++ WebContent/www2/src/controllers/boatController.js
@@ -1,6 +1,11 @@
 app.controller("BoatController", function($scope, $rootScope, BoatService) {
+	var defaultPageTitle = "Csónakok, katamarán";
+	var descLength = 20;
+	$scope.descCollapse = false;
+	
 	$scope.boats = [];
 	$scope.boat = null;
+	$scope.descCollapse = false;
 	
 	$rootScope.changePage("Csónakok, katamarán",null);
 	$rootScope.changeMeta("Csónakok", null,null)
@@ -13,10 +18,20 @@ app.controller("BoatController", function($scope, $rootScope, BoatService) {
 	
 	$scope.select = function(boat) {
 		$scope.boat = boat;
+		if(boat.desc_hu.length > descLength) {
+			$scope.descCollapse = true;
+			$scope.txt1 = boat.desc_hu.substring(0,descLength);
+			$scope.txt2 = boat.desc_hu.substring(descLength,boat.desc_hu.length);
+		} else {
+			$scope.txt1 = boat.desc_hu;
+		}
+		$rootScope.changePage(boat.name,null);
 	}
 	
 	$scope.back = function() {
+		$rootScope.changePage(defaultPageTitle,null);
 		$scope.boat = null;
+		$scope.descCollapse = false;
 	}
 	
 	$scope.refresh();
