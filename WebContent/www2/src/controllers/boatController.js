a1a9eee86518cf60afebaf6662436e28ca2bb306
cdog <cdog@cdog-laptop>
2015-11-03 22:12:12 +0100

    description text

--- WebContent/www2/src/controllers/boatController.js
+++ WebContent/www2/src/controllers/boatController.js
@@ -1,6 +1,9 @@
-app.controller("BoatController", function($scope, $rootScope, BoatService) {
+app.controller("BoatController", function($scope, $rootScope, $location, BoatService) {
 	var defaultPageTitle = "Csónakok, katamarán";
-	var descLength = 20;
+	var descLength = 500;
+	
+	$scope.descToggle=false;
+	$scope.descButtonExpandText="Bővebben..."
 	$scope.descCollapse = false;
 	
 	$scope.boats = [];
@@ -13,17 +16,26 @@ app.controller("BoatController", function($scope, $rootScope, BoatService) {
 	$scope.refresh = function() {
 		BoatService.getBoats().then(function(response) {			
 			$scope.boats = response.data;
+			//console.log($scope.boats);
+			
+			var boatId = $location.search().boatId
+			if( boatId != null) {
+				if($scope.boats[boatId] != null) {
+					$scope.select($scope.boats[boatId]);
+				}
+			}
 		});
+		
 	}
 	
 	$scope.select = function(boat) {
 		$scope.boat = boat;
 		if(boat.desc_hu.length > descLength) {
 			$scope.descCollapse = true;
-			$scope.txt1 = boat.desc_hu.substring(0,descLength);
-			$scope.txt2 = boat.desc_hu.substring(descLength,boat.desc_hu.length);
+			$scope.txt = boat.desc_hu.substring(0,descLength);
+			$scope.txtFull = boat.desc_hu;
 		} else {
-			$scope.txt1 = boat.desc_hu;
+			$scope.txtFull = boat.desc_hu;
 		}
 		$rootScope.changePage(boat.name,null);
 	}
@@ -32,6 +44,15 @@ app.controller("BoatController", function($scope, $rootScope, BoatService) {
 		$rootScope.changePage(defaultPageTitle,null);
 		$scope.boat = null;
 		$scope.descCollapse = false;
+	}
+	
+	$scope.toggleDesc = function() {
+		$scope.descToggle = !$scope.descToggle;
+		if($scope.descToggle) {
+			$scope.descButtonExpandText="Bezár..."
+		} else {
+			$scope.descButtonExpandText="Bővebben..."
+		}
 	}
 	
 	$scope.refresh();
