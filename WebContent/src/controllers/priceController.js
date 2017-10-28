db3bfcdbfd107a113a08489dd5ee8b209cc6519e
lestarcdog <lestarcdog@yahoo.com>
2017-10-28 20:12:07 +0200

    Price changes 2018

--- WebContent/src/controllers/priceController.js
+++ WebContent/src/controllers/priceController.js
@@ -1,8 +1,4 @@
-app.controller("PriceController", function($scope, $rootScope, ProductCategory, PriceService, BoatService, StorageConstants) {
-    var categories = {};
-    $scope.items = items = {};
-    $scope.search = {};
-    $scope.categoryNames = ProductCategory;
+app.controller("PriceController", function($scope, $rootScope, StorageConstants) {
 
     $rootScope.$on("langChanged", function(event, newLang) {
         changePageAndMeta();
@@ -19,56 +15,5 @@ app.controller("PriceController", function($scope, $rootScope, ProductCategory, 
         }
     };
 
-    var refresh = function() {
-        changePageAndMeta();
-        categories = {};
-        angular.forEach(ProductCategory, function(value, key) {
-            categories[key] = {
-                "name_hu" : value.name_hu.substring(0, 20),
-                "name_en" : value.name_en.substring(0, 20),
-                "value" : true
-            };
-            items[key] = [];
-        });
-        sortItems();
-        $scope.items = items;
-        $scope.reset();
-    };
-
-    var sortItems = function() {
-        // add boats before
-        BoatService.getBoats().then(function(response) {
-            angular.forEach(response.data, function(boat, key) {
-                if (boat.available) {
-                    items.boats.push({
-                        "name_hu" : boat.name,
-                        "name_en" : boat.name,
-                        "price" : boat.price
-                    });
-                }
-            });
-        });
-        PriceService.getPrices().then(function(response) {
-            angular.forEach(response.data, function(value, key) {
-                items[value.type].push({
-                    "name_hu" : value.name_hu,
-                    "name_en" : value.name_en,
-                    "price" : value.price
-                });
-            });
-        });
-    };
-
-    $scope.reset = function() {
-        $scope.search = {};
-        $scope.search.categories = angular.copy(categories);
-    };
-
-    $scope.disable = function() {
-        angular.forEach($scope.search.categories, function(v, key) {
-            v.value = false;
-        });
-    };
-
-    refresh();
+    changePageAndMeta();
 });
--- WebContent/src/service/priceService.js
+++
@@ -1,9 +0,0 @@
-app.factory("PriceService", function($http, httpEndpoints) {
-	var getPrices = function() {
-		return $http.get(httpEndpoints.prices);
-	};
-
-	return {
-		getPrices : getPrices
-	};
-});
